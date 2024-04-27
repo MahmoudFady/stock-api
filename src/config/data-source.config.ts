c52f4345c6b8578822cfd7bb8a512db514c5ba33
Mahmoud Fady <mahmoudfady02@gmail.com>
2024-04-28 02:12:19 +0300

    - add order and order-items functions

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -5,6 +5,8 @@ import { Employee } from "../entities/employee.entity";
 import { Lookup } from "../entities/lookup.entity";
 import { LookupDetail } from "../entities/lookup-details.entity";
 import { LookupGroup } from "../entities/lookup-group.entity";
+import { IncoomingOrder } from "../entities/incooming-order.entity";
+import { IncoomingOrderItem } from "../entities/incooming-order-item.entity";
 export const DataSourceConfig = new DataSource({
   type: "mysql",
   host: appEnv.DB_HOST,
@@ -14,7 +16,7 @@ export const DataSourceConfig = new DataSource({
   database: appEnv.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: [LookupGroup, Lookup, LookupDetail, Employee, Supplier],
+  entities: [LookupGroup, Lookup, LookupDetail, Employee, Supplier , IncoomingOrder , IncoomingOrderItem],
   subscribers: [],
   migrations: [],
 });
